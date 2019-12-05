44e46f3d83ac424fd08161f00e4d5b049034b57d
ji-water <bjisoo72@gmail.com>
2019-12-05 12:10:10 +0900

    audio 설정 코드 수정중

---
+++ Assets/JLBH_Scripts/AudioManage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManage : MonoBehaviour
+{
+    GameObject Listener;
+    GameObject AudioManager;
+
+    AudioSource AudioPlay;
+
+    static private float bgmVol; //0~1f 사이
+    static private float effetVol;
+
+    //Story
+
+    //Title
+    AudioClip TitleBGM;
+
+    //Menu
+    AudioClip MenuBGM;
+
+    //MainGame
+    AudioClip MainBGM;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        //bgm
+        //MainBGM = Resources.Load<AudioClip>("Sounds/Main/Main_BGM");
+
+        //play
+        switch (SceneManager.GetActiveScene().name)
+        {
+            // case "Story":
+            //...
+        }
+    }
+
+    public void setVol(float b,float e)
+    {
+        bgmVol = b;
+        effetVol = e;
+    }
+    public void changeVol()
+    {
+        AudioPlay.volume = bgmVol;
+    }
+}
